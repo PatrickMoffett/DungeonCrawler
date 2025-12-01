34013cf00cd14733c7c56cb4e960cb1cfdc7ca37
Patrick Moffett <patrick@moffett.dev>
2025-11-30 19:17:42 -0800

    movement cleanup

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,52 +4,69 @@ using UnityEngine.Serialization;
 
 namespace Player
 {
-    [RequireComponent(typeof(Rigidbody2D))]
-    public class PlayerMovement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 _velocity;
+    private Vector2 _inputDirection;
+    private Rigidbody2D _rigidbody;
+    
+    [SerializeField]
+    private float acceleration = 25f;
+    [SerializeField]
+    private float maxSpeed = 25f;
+    [SerializeField]
+    private float dampening = 25f;
+
+    private void Awake()
     {
-        private Vector2 _velocity;
-        private Vector2 _inputDirection;
-        private Rigidbody2D _rigidbody;
-        
-        [SerializeField]
-        private float acceleration = 25f;
-        [SerializeField]
-        private float maxSpeed = 25f;
-        [SerializeField]
-        private float dampening = 25f;
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
 
-        private void Awake()
+    private void Update()
+    {
+        UpdateVelocity();
+    }
+
+    private void UpdateVelocity()
+    {
+        var v = _rigidbody.linearVelocity;
+        var dt = Time.deltaTime;
+
+        if (Mathf.Approximately(_inputDirection.x,0f)
+            || (v.x > 0f && _inputDirection.x < 0f)
+            || (v.x < 0f && _inputDirection.x > 0f))
         {
-            _rigidbody = GetComponent<Rigidbody2D>();
-        }
-
-        private void Update()
-        {
-            UpdateVelocity();
-        }
-
-        private void UpdateVelocity()
-        {
-            var v = _rigidbody.linearVelocity;
-            var dt = Time.deltaTime;
-
-            v += _inputDirection * (acceleration * dt);
-
+            Debug.Log("DampendingX");
             var brake = dampening * dt;
-            float newSpeed = v.magnitude - brake;
-            if (newSpeed < 0f)
-            {
-                newSpeed = 0f;
-            }
-            v = v.normalized * newSpeed;
-            
-            _rigidbody.linearVelocity = v;
+            float newSpeed = Mathf.MoveTowards(v.x, 0f, brake);
+            v.x = newSpeed;
         }
-        
-        public void SetMoveInput(Vector2 moveDirection)
+        else
         {
-            Debug.Log("SetMoveInput: " + moveDirection.x);
-            _inputDirection = moveDirection;  
+            Debug.Log("Not DampendingX");
         }
+        if (Mathf.Approximately(_inputDirection.y,0f)
+            || (v.y > 0f && _inputDirection.y < 0f)
+            || (v.y < 0f && _inputDirection.y > 0f))
+        {
+            var brake = dampening * dt;
+            float newSpeed = Mathf.MoveTowards(v.y, 0f, brake);
+            v.y = newSpeed;
+        }
+        v += _inputDirection * (acceleration * dt);
+
+        if (v.sqrMagnitude > maxSpeed * maxSpeed)
+        {
+            v = v.normalized * maxSpeed;
+        }
+        _rigidbody.linearVelocity = v;
+    }
+    
+    public void SetMoveInput(Vector2 moveDirection)
+    {
+        Debug.Log("SetMoveInput: " + moveDirection.x);
+        _inputDirection = moveDirection;  
     }
 }
+} // namespace Player
